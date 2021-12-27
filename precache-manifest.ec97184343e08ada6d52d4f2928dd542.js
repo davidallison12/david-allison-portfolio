142d6bfa29355c33fd6a155626afed9c192a3388
david-allison12 <dgreenal@gmail.com>
2021-12-26 23:51:36 -0500

    Updates

---
+++ precache-manifest.ec97184343e08ada6d52d4f2928dd542.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9c41a168158a184e70991f24a129ba4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8a3c83413af8d9523c9",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "7017d0a8221df2976d15",
+    "url": "/static/css/main.0b86f9d6.chunk.css"
+  },
+  {
+    "revision": "d8a3c83413af8d9523c9",
+    "url": "/static/js/2.239c13d5.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.239c13d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7017d0a8221df2976d15",
+    "url": "/static/js/main.9c7c640c.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
